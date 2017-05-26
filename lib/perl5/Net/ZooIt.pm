9dac74a4e436c04acaa55feed689150f064f68b2
SZABO Gergely <szg@subogero.com>
2017-05-26 08:49:42 +0200

    Add ZooKeeper recipe Lock with test case

--- lib/perl5/Net/ZooIt.pm
+++ lib/perl5/Net/ZooIt.pm
@@ -44,4 +44,75 @@ sub zwarn { logger ZOOIT_WARN, @_ }
 sub zinfo { logger ZOOIT_INFO, @_ }
 sub zdebug { logger ZOOIT_DEBUG, @_ }
 
+sub zerr2txt {
+    my $err = shift;
+    our %code2name;
+    unless (%code2name) {
+        foreach my $name (@{$Net::ZooKeeper::EXPORT_TAGS{errors}}) {
+            no strict "refs";
+            my $code = &$name;
+            use strict "refs";
+            $code2name{$code} = $name;
+        }
+    }
+    return $code2name{$err};
+}
+
+# ZooKeeper recipe Lock
+sub new_lock {
+    my $class = shift;
+    zerr "lock will be released immediately, new_lock called in void context"
+        unless defined wantarray;
+    my %p = @_;
+    zdie "Param zk must be a connect Net::ZooKeeper object"
+        unless ref $p{zk};
+    zdie "Param path must be a valid ZooKeeper znode path"
+        unless $p{path} =~ m|^/.+|;
+    $p{blocking} //= 1;
+
+    my $lock = $p{zk}->create(
+        "$p{path}/lock-" => hostname . " PID $$",
+        flags => ZOO_EPHEMERAL|ZOO_SEQUENCE,
+        acl => ZOO_OPEN_ACL_UNSAFE,
+    ) or zdie "Could not create $p{path}/lock-: " . zerr2txt($p{zk}->get_error);
+    zinfo "Created lock $lock";
+
+    my ($basename, $n) = split /-/, $lock;
+    while (1) {
+        zdebug "Get lock list:";
+        my @locks = $p{zk}->get_children($p{path});
+        my $n_prev;
+        # Look for other lock with highest sequence number lower than mine
+        foreach (@locks) {
+            zdebug "  $_";
+            my ($basename_i, $n_i) = split /-/;
+            next if $n_i >= $n;
+            $n_prev = $n_i if !defined $n_prev || $n_i > $n_prev;
+        }
+        # If none found, the lock is mine
+        unless (defined $n_prev) {
+            zinfo "Take lock: $lock";
+            return bless { lock => $lock, zk => $p{zk} };
+        }
+        # I can't take lock, abort if non-blocking call
+        unless ($p{blocking}) {
+            zinfo "Non-blocking call, abort";
+            $p{zk}->delete($lock);
+            return;
+        }
+        # Wait for lock with highest seq number lower than mine to be deleted
+        my $w = $p{zk}->watch(timeout => 10000);
+        $w->wait if $p{zk}->exists("$p{path}/lock-$n_prev", watch => $w);
+    }
+}
+
+sub DESTROY {
+    my $self = shift;
+    if ($self->{lock}) {
+        zinfo "Release lock: $self->{lock}";
+        $self->{zk}->delete($self->{lock});
+        delete $self->{lock};
+    }
+}
+
 1;
---
+++ t/lock.t
@@ -0,0 +1,49 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+use FindBin;
+use lib "$FindBin::Bin/../lib/perl5";
+use Net::ZooIt;
+use Net::ZooKeeper qw(:all);
+use Test::More;
+
+$| = 1;
+Net::ZooIt::set_log_level(ZOOIT_DEBUG);
+
+my $file = 'incr.txt';
+unlink $file;
+
+my $url = shift // '127.0.0.1:2181';
+
+my $pid = fork;
+my $zk = Net::ZooKeeper->new($url);
+$zk->create('/zooitlock' => $$, acl => ZOO_OPEN_ACL_UNSAFE);
+
+incr() for 0 .. 9;
+$pid ? wait : exit;
+$zk->delete('/zooitlock');
+
+open FILE, $file or die $!;
+my $n;
+(undef, $n) = split /\s/ while <FILE>;
+close FILE;
+ok($n == 20, 'n incrmented to 20, races avoided');
+
+done_testing;
+
+sub incr {
+    my $lock = Net::ZooIt->new_lock(zk => $zk, path => '/zooitlock');
+    my $num;
+    if (open FILE, $file) {
+        (undef, $num) = split /\s/ while <FILE>;
+        close FILE;
+    }
+    $num //= 0;
+    $num++;
+    sleep 1;
+    open FILE, ">>", $file or die $!;
+    print STDERR "$$ $num\n";
+    print FILE "$$ $num\n";
+    close FILE;
+}
